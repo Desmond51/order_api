981e5cc1a4655c6b3ff145119b5931796a692d88
Desmond51 <desmondtekum9@gmail.com>
2024-03-17 04:42:38 +0100

    used chi router

--- main.go
+++ main.go
@@ -4,15 +4,20 @@ import (
 	"fmt"
 	"net/http"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 
 
 
 func  main()  {
+	router := chi.NewRouter()
+	router.Get("/hello", basicHandler)
+	router.Use(middleware.Logger)
+
 	server := &http.Server{
 		Addr: ":3000",
-		Handler: http.HandlerFunc(basicHandler),
+		Handler: router,
 	}
 	err := server.ListenAndServe()
 	if err != nil{
